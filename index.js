a6411c14de4716f79a7f2ffa4d649ef55a196ae9
Victoria Meng <nights1ststar@protonmail.com>
2018-08-13 23:23:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(block) {
-  return Math.abs(42 - block);
+  return Math.abs(42 - block)
+}
+
+function distanceFromHqInFeet(block) {
+  
 }
